cf57b33d73f7b8dae85a496486447792b4ae17b2
smetdenis <admin@jbzoo.com>
2015-07-03 18:31:08 +0600

    New autoload for tests

---
+++ tests/autoload.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * SimpleTypes
+ *
+ * Copyright (c) 2015, Denis Smetannikov <denis@jbzoo.com>.
+ *
+ * @package    SimpleTypes
+ * @author     Denis Smetannikov <denis@jbzoo.com>
+ * @copyright  2015 Denis Smetannikov <denis@jbzoo.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * @link       http://github.com/smetdenis/simpletypes
+ */
+
+// @codingStandardsIgnoreFile
+// @codeCoverageIgnoreStart
+
+// main autoload
+require_once 'src/autoload.php';
+
+// test tools
+require_once 'configs.php';
+require_once 'type/typeTest.php';
+
+
+// @codeCoverageIgnoreEnd
