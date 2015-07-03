c1136ffcfc1c0cb12c4b06af6690dbb8b619a88e
smetdenis <admin@jbzoo.com>
2015-07-03 13:36:03 +0600

    Tests for info type

--- tests/type/infoTypeTest.php
+++ tests/type/infoTypeTest.php
@@ -15,20 +15,41 @@ class infoTypeTest extends typeTest
     protected $_type = 'info';
 
 
-    function testCreate()
+    function testSimple()
     {
-        $val = $this->_('1024Gb')
-                    ->convert('bit')
-                    ->convert('kb')
-                    ->convert('byte')
-                    ->convert('bit')
-                    ->convert('bit')
-                    ->convert('byte')
-                    ->convert('kb')
-                    ->convert('gb');
+        $this->_batchEquals(array(
+            ['1 KB', $this->_('1024 byte')->text('KB')],
+            ['1 KB', $this->_('8192 bit')->text('KB')],
+            ['4 GB', $this->_('4294967296 byte')->text('GB')],
+            ['32 B', $this->_('256 bit')->text('Byte')],
+        ));
+    }
 
-        $this->assertEquals('1024 gb', $val->dump(false));
+    function testConvert()
+    {
+        $this->_batchEquals(array(
+            ['81920 bit', $this->_('10Kb')->convert('bit')->dump(false)],
+            ['10 kb', $this->_('81920bit')->convert('mb')->convert('kb')->dump(false)]
+        ));
+    }
 
+    function testCompare()
+    {
+        $this->assertTrue($this->_('10kb')->compare('81920bit'));
+    }
+
+    function testComplex()
+    {
+        $val = $this->_('81920 bit')
+            ->subtract('9 KB')
+            ->multiply(5)
+            ->convert('KB')
+            ->convert('bit')
+            ->multiply(1024)
+            ->add('512 kb')
+            ->convert('mb');
+
+        $this->assertEquals('5.5 mb', $val->dump(false));
     }
 
 
