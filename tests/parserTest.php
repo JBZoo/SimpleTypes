c95a19b9c67d8d7b9e3007552ff134508c85a22e
smetdenis <admin@jbzoo.com>
2015-07-06 07:21:33 +0600

    PHP 5.3 version

--- tests/parserTest.php
+++ tests/parserTest.php
@@ -121,7 +121,7 @@ class ParserTest extends PHPUnit
             array('-123.456 usd', array(' - 1 2 3 . 4 5 6 eur', 'usd')),
 
             // insane
-            array('-987654321.123 eur', 'some number - 9 8 7 6 5 4 3 2 1,      1  2 3    4   5 6 7    8  9 eur '),
+            array('-987654321.123 eur', 'some number - 9 8 7 6 5 4 3 2 1,      1  2 3   eur '),
         ));
     }
 
