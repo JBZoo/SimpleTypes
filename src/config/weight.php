0f221506c0a86ae227a86ffc65407555bc55831b
smetdenis <admin@jbzoo.com>
2015-07-11 04:31:55 +0600

    Weight config and tests

--- src/config/weight.php
+++ src/config/weight.php
@@ -1,7 +1,9 @@
 <?php
 /**
  * SimpleTypes
+ *
  * Copyright (c) 2015, Denis Smetannikov <denis@jbzoo.com>.
+ *
  * @package   SimpleTypes
  * @author    Denis Smetannikov <denis@jbzoo.com>
  * @copyright 2015 Denis Smetannikov <denis@jbzoo.com>
@@ -26,57 +28,33 @@ class ConfigWeight extends Config
     {
         return array(
             // SI
-            'g'         => array(
+            'g'     => array(
                 'symbol' => 'g',
                 'rate'   => 1,
             ),
-            'kg'        => array(
+            'kg'    => array(
                 'symbol' => 'Kg',
                 'rate'   => 1000,
             ),
-
-            // tons
-            'short-ton' => array(
-                'symbol' => 'Sh.Tons',
-                'rate'   => 907184.74,
-            ),
-            'ton'       => array(
+            'ton'   => array(
                 'symbol' => 'Tons',
                 'rate'   => 1000000,
             ),
-            'long-ton'  => array(
-                'symbol' => 'Long.Tons',
-                'rate'   => 1016046.9088,
-            ),
-
-            // Hundredweight
-            'short-ct'  => array(
-                'symbol' => 'Sh.Centums',
-                'rate'   => 45359.237,
-            ),
-            'ct'        => array(
-                'symbol' => 'Centum',
-                'rate'   => 100000,
-            ),
-            'long-ct'   => array(
-                'symbol' => 'Long.Centums',
-                'rate'   => 50802.345,
-            ),
 
             // other
-            'gr'        => array(
+            'gr'    => array(
                 'symbol' => 'Grains',
                 'rate'   => 0.06479891,
             ),
-            'dr'        => array(
+            'dr'    => array(
                 'symbol' => 'Drams',
                 'rate'   => 1.7718451953125,
             ),
-            'oz'        => array(
+            'oz'    => array(
                 'symbol' => 'Ounces',
                 'rate'   => 28.349523125,
             ),
-            'lb'        => array(
+            'lb'    => array(
                 'symbol' => 'Pounds',
                 'rate'   => 0.06479891,
             ),
---
+++ tests/type/weightTypeTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * SimpleTypes
+ *
+ * Copyright (c) 2015, Denis Smetannikov <denis@jbzoo.com>.
+ *
+ * @package   SimpleTypes
+ * @author    Denis Smetannikov <denis@jbzoo.com>
+ * @copyright 2015 Denis Smetannikov <denis@jbzoo.com>
+ * @link      http://github.com/smetdenis/simpletypes
+ */
+
+namespace SmetDenis\SimpleTypes;
+
+/**
+ * Class WeightTypeTest
+ * @package SmetDenis\SimpleTypes
+ */
+class WeightTypeTest extends typeTest
+{
+
+    protected $type = 'weight';
+
+    public function testSimple()
+    {
+        $weight = $this->val('10000g');
+
+        // SI
+        $this->batchEquals(array(
+            array('10000 g', $weight->dump(false)),
+            array('10 kg', $weight->convert('kg')->dump(false)),
+            array('0.01 ton', $weight->convert('ton')->dump(false)),
+        ));
+
+        $this->batchEquals(array(
+            array('0.06479891 g', $this->val('1 gr')->convert('g')->dump(false)),
+            array('1.7718451953125 g', $this->val('1 dr')->convert('g')->dump(false)),
+            array('28.349523125 g', $this->val('1 oz')->convert('g')->dump(false)),
+            array('0.06479891 g', $this->val('1 lb')->convert('g')->dump(false)),
+        ));
+    }
+}
